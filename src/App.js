dfcb0fd9fba5e3f731f021676ae018a62c875a9f
Monica <monica.parrillo@gmail.com>
2020-12-06 17:01:27 -0600

    Add style to home, layout

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 import Home from './components/Home'
 import Starship from './components/Starship'
+import Layout from './components/common/Layout'
 import './styles/App.css';
 
 const App = () => {
   return (
     <div className="App">
-        <Route exact path="/" component={Home} />
-        <Route path='/starship' render={({location}) =>
+      <Layout>
+        <div className="main">
+          <Route exact path="/" component={Home} />
+          <Route path='/starship' render={({location}) =>
               <Starship location={location}/>
             }/>
+          </div>
+      </Layout>
     </div>
   );
 }
